e77608f510c0f079395e2a59f02c3fe32b5aeeeb
pqml <hugo.piquemal@stinkstudios.com>
2018-10-08 16:28:06 +0200

    Add SVG support

---
+++ demo/Svg.js
@@ -0,0 +1,75 @@
+import { h } from '../lib'
+
+export default function Svg () {
+  return (
+    <div>
+      <br />
+      <h4>SVG support:</h4>
+      <svg version='1.1' id='Calque_1' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink' width='150px' x='0px' y='0px' viewBox='0 0 192 205' xmlSpace='preserve' enable-background='new 0 0 192 205'>
+        <g>
+          <defs>
+            <path id='SVGID_1_' d='M53,1h86c29.3,0,53,23.7,53,53v98c0,29.3-23.7,53-53,53H53c-29.3,0-53-23.7-53-53V54C0,24.7,23.7,1,53,1z' />
+          </defs>
+          <clipPath id='SVGID_2_'>
+            <use xlinkHref='#SVGID_1_' overflow='visible' />
+          </clipPath>
+          <rect x='-5' y='-4' width='202' height='214' clip-path='url(#SVGID_2_)' fill='#f4f4f4' />
+        </g>
+        <g>
+          <defs>
+            <rect id='SVGID_3_' x='31.9' y='30.8' width='52.5' height='52.3' />
+          </defs>
+          <clipPath id='SVGID_4_'>
+            <use xlinkHref='#SVGID_3_' overflow='visible' />
+          </clipPath>
+          <rect x='26.9' y='25.8' width='62.5' height='62.3' clip-path='url(#SVGID_4_)' fill='#fdaa02' />
+        </g>
+        <g>
+          <defs>
+            <polygon id='SVGID_5_' points='137.5,31 164,84 111,84' />
+          </defs>
+          <clipPath id='SVGID_6_'>
+            <use xlinkHref='#SVGID_5_' overflow='visible' />
+          </clipPath>
+          <g clip-path='url(#SVGID_6_)' enable-background='new'>
+            <g>
+              <defs>
+                <rect id='SVGID_7_' x='84' y='4' width='107' height='107' />
+              </defs>
+              <clipPath id='SVGID_8_'>
+                <use xlinkHref='#SVGID_7_' overflow='visible' />
+              </clipPath>
+              <g clip-path='url(#SVGID_8_)'>
+                <defs>
+                  <rect id='SVGID_9_' x='111' y='31' width='54' height='54' />
+                </defs>
+                <clipPath id='SVGID_10_'>
+                  <use xlinkHref='#SVGID_9_' overflow='visible' />
+                </clipPath>
+                <rect x='106' y='26' width='63' height='63' clip-path='url(#SVGID_10_)' fill='#fff' />
+              </g>
+              <g clip-path='url(#SVGID_8_)'>
+                <defs>
+                  <rect id='SVGID_11_' x='95.2' y='15.2' transform='matrix(0.9511 -0.309 0.309 0.9511 -10.9749 45.2164)' width='84' height='84' />
+                </defs>
+                <clipPath id='SVGID_12_'>
+                  <use xlinkHref='#SVGID_11_' overflow='visible' />
+                </clipPath>
+                <g clip-path='url(#SVGID_12_)'>
+                  <defs>
+                    <rect id='SVGID_13_' x='111' y='31' width='54' height='54' />
+                  </defs>
+                  <clipPath id='SVGID_14_'>
+                    <use xlinkHref='#SVGID_13_' overflow='visible' />
+                  </clipPath>
+                  <rect x='84.2' y='21.2' transform='matrix(0.9511 -0.309 0.309 0.9511 -14.6678 43.9006)' width='94' height='94' clip-path='url(#SVGID_14_)' />
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <path fill='none' stroke='#000' stroke-miterlimit='10' d='M70.6 160.2c-1.1-2.1-1.5-4.4-1.4-6.9l-8.4 4.1 9.8 2.8zm32.1-43.7C92.2 120 84 127.2 75.8 138.2c-1 1.3-1.9 2.7-2.6 3.9-1.8.1-3.7-.2-5.6-1-15.9-6.8 21.6-18.3 10.8-24.7-1.6-1-3.2-1.7-4.7-2.2L55 140.7l15.6 6.7c.6-1.7 1.5-3.5 2.6-5.3 12.7-.8 20.7-22 23.2-14.6.8 2.4 1.9 5.9 3 9.9 11.3-9 19.7-16.8 25.2-23.2 1.3.1 2.7.2 4.1.4 5.2.6 9.2 1.1 12.2 1.6 2.6 4.5 5.4 10.7 8.4 18.6-1 4.4-3.2 9.7-6.7 14.9l-32.4 14.8-6.9-3c0 2.7-.1 5.3-.5 7.7l5.7 1.6c10-1.2 18.1-5.2 24.5-10.3l-2.2-26-28.1-18zm-.1 52.7l-32-8.9c3.6 6.8 13.9 10.9 31.6 10.9.1-.3.2-.5.2-.8.1-.5.2-.9.2-1.2zm-3.2-31.7c-1.3 1.1-2.7 2.2-4.2 3.3l-26 12.6c.1-1.9.6-3.8 1.4-5.9l32.6 14.1c-.1-8.1-2-17-3.8-24.1zm3.3-21c6.3-2.1 13.4-2.8 21.9-2.2 1.4-1.7 2.6-3.3 3.7-4.8 3.2-4.7 7.4-2.5 12.6 6.8 11.3 1.9 9 4.3 9 13.9 0 1.4-.2 2.9-.6 4.6 1.2 3.1 2.5 6.5 3.8 10.2l-10.5 4.8c-2.5 3.8-5.8 7.5-9.7 10.7l1.5 17.6-25.9-7.2c-2 .2-4 .4-6.1.4-4.4 12.2-44.6 5.7-57.1-.8-12.8-6.7-6.5-28.4-6.5-31.6 0-3.1 3.7-19.9 13.1-22.3 8-2 13.9-5.2 22.1-2.2l7.9-11.2 20.8 13.3z' />
+      </svg>
+    </div>
+  )
+}
