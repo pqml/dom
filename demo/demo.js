26ce94c3b95a09150946c49e45e17dbdd5b827ec
pqml <hugo.piquemal@stinkstudios.com>
2018-11-23 17:56:35 +0100

    Fix issues and invalid es5 code

--- demo/demo.js
+++ demo/demo.js
@@ -4,3 +4,13 @@ import Svg from './Svg'
 
 render(<Subtree base={document.querySelector('main')} />)
 render(<Svg />, document.body)
+render([
+  <h4>Inline video support:</h4>,
+  <video
+    width='300'
+    src='https://antoine.cool/content/2-projects/1-copilote/preview.mobile.mp4'
+    autoplay
+    muted
+    playsinline
+  />
+], document.body)
